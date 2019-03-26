d86adf8fa91ed8df169886cfb71f077f26cc4123
Sebastian Wilgosz <sebastian@driggl.com>
2019-03-26 09:35:28 +0100

    Add Error serializer
    Based on the provided error error serializer produces
    ready to render object according to jsonapi format.
    
    Refer: https://jsonapi.org/format/#errors

--- lib/jsonapi_errors_handler.rb
+++ lib/jsonapi_errors_handler.rb
@@ -1,5 +1,6 @@
 require "jsonapi_errors_handler/version"
 require "jsonapi_errors_handler/error_mapper"
+require "jsonapi_errors_handler/error_serializer"
 
 module JsonapiErrorsHandler
   # Your code goes here...
---
+++ lib/jsonapi_errors_handler/error_searializer.rb
@@ -0,0 +1,26 @@
+module JsonapiErrorsHandler
+  class ErrorSerializer
+    def initialize(error)
+      @error = error
+    end
+
+    def to_h
+      serializable_hash
+    end
+
+    def to_json(payload)
+      to_h.to_json
+    end
+
+    private
+
+    def serializable_hash
+      {
+        errors: Array.wrap(error.serializable_hash)
+      }
+    end
+
+    attr_reader :error
+  end
+end
+
